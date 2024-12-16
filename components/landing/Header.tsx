3206d4775e33bf80d7ba9189cec7405fb0da1593
Manas Kumar Pradhan <pkmanas22@gmail.com>
2024-12-16 08:02:13 +0530

    fix: dashboard navigation from landing page

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Logo from "../Logo";
+import { useSession } from "next-auth/react";
+import { ArrowRight } from "lucide-react";
 
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <header className="bg-white/[0.2] shadow-md bg-gray-700 bg-opacity-40">
       <nav className="container mx-auto px-6 py-3">
@@ -23,15 +28,26 @@ export default function Header() {
             </button>
           </div>
           <div className="flex space-x-2">
-            <Button asChild>
-              <Link href="/signup/helper">Donate Now</Link>
-            </Button>
-            <Button variant={"outline"} asChild>
-              <Link href="/signup/needer">Create Request</Link>
-            </Button>
-            <Button className="bg-blue-700 hover:bg-blue-600" asChild>
-              <Link href="/signin">Login</Link>
-            </Button>
+            {session?.user.id ? (
+              <>
+                <Button asChild>
+                  <Link href="/signup/helper">Donate Now</Link>
+                </Button>
+                <Button variant={"outline"} asChild>
+                  <Link href="/signup/needer">Create Request</Link>
+                </Button>
+                <Button className="bg-blue-700 hover:bg-blue-600" asChild>
+                  <Link href="/signin">Login</Link>
+                </Button>
+              </>
+            ) : (
+              <Button variant={"outline"} asChild>
+                <Link href="/dashboard">
+                  Go to Your Dashboard
+                  <ArrowRight />
+                </Link>
+              </Button>
+            )}
           </div>
         </div>
       </nav>
